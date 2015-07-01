1e3f6d36b352cc426c59b5b503dd1c509ec3d7d3
Bert Spaan <bert.spaan@gmail.com>
2015-07-01 12:29:05 +0200

    moved rules files to new dir

--- rules/tgn.rules.js
+++ rules/tgn.rules.js
@@ -24,7 +24,23 @@ module.exports = {
       filter: function(pit) {
         // Do not use TGN term objects
         return pit.uri.indexOf('term') == -1;
-      }
+      },
+      override: [
+        // Force inferred relation between the following three provinces:
+        // (This may not be needed later when proper ES tokenizer is in place)
+        {
+          from: 'http://vocab.getty.edu/tgn/7003632',
+          to: 'http://sws.geonames.org/2743698'
+        },
+        {
+          from: 'http://vocab.getty.edu/tgn/7003624',
+          to: 'http://sws.geonames.org/2749990'
+        },
+        {
+          from: 'http://vocab.getty.edu/tgn/7006951',
+          to: 'http://sws.geonames.org/2749879'
+        }
+      ]
     },
 
     {
